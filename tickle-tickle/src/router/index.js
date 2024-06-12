a6a43f5ab9f3e687e9470a6fbffc8fd539d1b8a9
Hongse0 <hongseyoung981007@gmail.com>
2024-06-12 14:48:33 +0900

    충돌 병합+수정

--- tickle-tickle/src/router/index.js
+++ tickle-tickle/src/router/index.js
@@ -4,12 +4,10 @@ import Investment from "../views/investment/Investment.vue";
 import ChallengeList from "../views/challenge/ChallengeList.vue";
 import ChallengeCreate from "../views/challenge/ChallengeCreate.vue";
 import Userstats from "../views/stats/dashboards/Userstats.vue";
-<<<<<<< HEAD
-import NewTransaction from "../views/Transaction/NewTransaction.vue";
+
+import NewTransaction from "../views/transaction/NewTransaction.vue"
 import Starstats from "../views/stats/dashboards/Starstats.vue";
-=======
-import NewTransaction from "../views/transaction/NewTransaction.vue";
->>>>>>> 0e4e4b9d30971a1d4cbc54f0dc6770c1445af310
+
 // import lockIllustration from "../views/auth/lock/Illustration.vue";
 
 
@@ -45,15 +43,15 @@ const routes = [
     name: "NewTransaction",
     component: NewTransaction
   },
-<<<<<<< HEAD
+
   {
     path: "/stats/stars",
     name: "stars",
     component: Starstats
   },
-=======
 
->>>>>>> 0e4e4b9d30971a1d4cbc54f0dc6770c1445af310
+
+
 ];
 
 const router = createRouter({
