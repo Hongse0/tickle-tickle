b4cc8690071f36a42a45f87a2cfb5ce14b07e536
Hongse0 <hongseyoung981007@gmail.com>
2024-06-12 10:41:49 +0900

    홍세영/충돌수정/0612

--- tickle-tickle/src/router/index.js
+++ tickle-tickle/src/router/index.js
@@ -1,12 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
-<<<<<<< HEAD
-import Investment from "../views/Investment.vue";
 import Userstats from "../views/stats/dashboards/Userstats.vue";
-=======
 import Investment from "../views/investment/Investment.vue";
->>>>>>> 028f388689bcc553ce063564a071d840567c955a
 // import lockIllustration from "../views/auth/lock/Illustration.vue";
 
 const routes = [
