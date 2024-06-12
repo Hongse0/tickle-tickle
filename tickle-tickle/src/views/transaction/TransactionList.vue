097c53682758cfd9ee4bf7f4452b2cc5d0b3797b
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-06-12 16:20:28 +0900

    곽민지/거래조회 상단 월별 합계 추가

--- tickle-tickle/src/views/transaction/TransactionList.vue
+++ tickle-tickle/src/views/transaction/TransactionList.vue
@@ -6,18 +6,61 @@ import Calendar from "@/examples/Calendar.vue";
 <template>
   <div class="py-4 container-fluid">
     <div class="row">
-      <div class="col-12 ms-auto">
-        <div class="mb-4 d-flex">
-          <div class="ps-4">
-            <button
-              class="mt-3 mb-0 btn btn-outline-white btn-icon-only"
-              data-bs-toggle="modal"
-              data-target="#new-board-modal"
-            >
-              <i class="fa fa-plus text-white"></i>
-            </button>
+      <div class="col-12 ms-auto mb-5">
+        <div class="card-group">
+          <div class="card">
+            <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
+              
+            </div>
+        
+            <div class="card-body pt-2">
+              <a href="javascript:;" class="card-title h5 d-block text-darker">
+                월별 총합
+              </a>
+              <p class="card-description mb-4">
+                121530원
+              </p>
+              <a href="javascript:;" >
+                <img src="../../assets/img/transAll.png" class="img-fluid border-radius-lg">
+              </a>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
+              
+            </div>
+        
+            <div class="card-body pt-2">
+              <a href="javascript:;" class="card-title h5 d-block text-darker">
+                월별 수익
+              </a>
+              <p class="card-description mb-4">
+                105000원
+              </p>
+              <a href="javascript:;" class="">
+                <img src="../../assets/img/transIncome.png" class="img-fluid border-radius-lg">
+              </a>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
+              
+            </div>
+        
+            <div class="card-body pt-2">
+              <a href="javascript:;" class="card-title h5 d-block text-darker">
+                월별 지출
+              </a>
+              <p class="card-description mb-4">
+                145000원
+              </p>
+              <a href="javascript:;" class="">
+                <img src="../../assets/img/transExpend.png" class="img-fluid border-radius-lg">
+              </a>
+            </div>
           </div>
         </div>
+        
       </div>
       <div class="col-xl-12">
         <calendar id="calendar-table" />
