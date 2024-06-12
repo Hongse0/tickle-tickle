a7c3bb7e5ff9f598a10b37bde069edcc6673dbcc
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-06-12 17:24:42 +0900

    곽민지/거래조회 금액으로 변경

--- tickle-tickle/src/examples/Calendar.vue
+++ tickle-tickle/src/examples/Calendar.vue
@@ -28,80 +28,53 @@ const props = defineProps({
   },
   initialDate: {
     type: String,
-    default: "2020-12-01",
+    default: "2024-06-01",
   },
   events: {
     type: Array,
     default: () => [
       {
-        title: "Call with Dave",
-        start: "2020-11-18",
-        end: "2020-11-18",
-        className: "bg-gradient-danger",
-      },
-
-      {
-        title: "Lunch meeting",
-        start: "2020-11-21",
-        end: "2020-11-22",
-        className: "bg-gradient-warning",
-      },
-
-      {
-        title: "All day conference",
-        start: "2020-11-29",
-        end: "2020-11-29",
-        className: "bg-gradient-success",
-      },
-
-      {
-        title: "Meeting with Mary",
-        start: "2020-12-01",
-        end: "2020-12-01",
-        className: "bg-gradient-info",
-      },
-
-      {
-        title: "Winter Hackaton",
-        start: "2020-12-03",
-        end: "2020-12-03",
-        className: "bg-gradient-danger",
-      },
-
-      {
-        title: "Digital event",
-        start: "2020-12-07",
-        end: "2020-12-09",
-        className: "bg-gradient-warning",
-      },
-
-      {
-        title: "Marketing event",
-        start: "2020-12-10",
-        end: "2020-12-10",
-        className: "bg-gradient-success",
-      },
-
-      {
-        title: "Dinner with Family",
-        start: "2020-12-19",
-        end: "2020-12-19",
-        className: "bg-gradient-danger",
-      },
-
-      {
-        title: "Black Friday",
-        start: "2020-12-23",
-        end: "2020-12-23",
-        className: "bg-gradient-info",
-      },
-
-      {
-        title: "Cyber Week",
-        start: "2020-12-02",
-        end: "2020-12-02",
-        className: "bg-gradient-warning",
-      },
+					"id" : 1,
+					"userId" :1,
+					"start" : "2024-06-11",
+					"price": 6000, 
+					"title": "Real MY SQL구입",
+					"memo": "책 엄청 비싸네;;",
+					"isIncome": true, 
+					"category": "쇼핑"
+				},
+				
+			{
+					"id" : 2,
+					"userId" :1,
+					"start" : "2024-06-12",
+					"price": 18000, 
+					"title": "vue.js 마스터 구입",
+					"memo": "뷰 마스터가 되어야지!",
+					"isIncome": true,  
+					"category": "쇼핑"
+				},
+				
+				{
+					"id" : 3,
+					"userId" :1,
+					"start" : "2024-06-10",
+					"price": 26000, 
+					"title": "Spring 시작 구입",
+					"memo": "어려워보인당...",
+					"isIncome": false, 
+					"category": "쇼핑"
+				},
+        {
+					"id" : 4,
+					"userId" :1,
+					"start" : "2024-06-11",
+					"price": 10000, 
+					"title": "Real MY SQL판매",
+					"memo": "안할래",
+					"isIncome": false, 
+					"category": "중고거래"
+				},
     ],
   },
   selectable: {
@@ -128,6 +101,7 @@ onMounted(() => {
       center: "",
       end: "today prev,next", // will normally be on the right. if RTL, will be on the left
     },
+    eventContent: eventContent,  // 추가
     views: {
       month: {
         titleFormat: {
@@ -160,6 +134,12 @@ onBeforeUnmount(() => {
     calendar.destroy();
   }
 });
+const eventContent = (info) => {
+  return {
+    html: `<div class="text-center">${info.event.extendedProps.price}</div>`
+  };
+};
+
 </script>
 <template>
   <div class="card widget-calendar">
