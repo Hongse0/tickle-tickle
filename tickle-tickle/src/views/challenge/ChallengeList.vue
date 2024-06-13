56cad4a37ea864789b367df42b9755784012d85c
gahee99 <u1879@sookmyung.ac.kr>
2024-06-13 14:23:36 +0900

    최가희/axios 받아옴

--- tickle-tickle/src/views/challenge/ChallengeList.vue
+++ tickle-tickle/src/views/challenge/ChallengeList.vue
@@ -1,100 +1,199 @@
 <template>
-  <div class="card col-lg-11 container justify-content-center">
-    <div class="row d-flex">
-      <!-- 카드  -->
+  <div class="container">
+    <br />
+    <!-- TODO -->
+    <!-- <div class="row col-lg-12">
+      <div class="col-lg-3 col-md-6 col-12">
+        <mini-statistics-card
+          title="Today's Money"
+          value="$53,000"
+          description="<span
+                class='text-sm font-weight-bolder text-success'
+                >+55%</span> {{ title }}"
+          :icon="{
+            component: 'ni ni-money-coins',
+            background: 'bg-gradient-primary',
+            shape: 'rounded-circle',
+          }"
+        />
+      </div>
+      <div class="col-lg-3 col-md-6 col-12">
+        <mini-statistics-card
+          title="Today's Users"
+          value="2,300"
+          description="<span
+                class='text-sm font-weight-bolder text-success'
+                >+3%</span> since last week"
+          :icon="{
+            component: 'ni ni-world',
+            background: 'bg-gradient-danger',
+            shape: 'rounded-circle',
+          }"
+        />
+      </div>
+      <div class="col-lg-3 col-md-6 col-12">
+        <mini-statistics-card
+          title="New Clients"
+          value="+3,462"
+          description="<span
+                class='text-sm font-weight-bolder text-danger'
+                >-2%</span> since last quarter"
+          :icon="{
+            component: 'ni ni-paper-diploma',
+            background: 'bg-gradient-success',
+            shape: 'rounded-circle',
+          }"
+        />
+      </div>
+      <div class="col-lg-3 col-md-6 col-12">
+        <mini-statistics-card
+          title="Sales"
+          value="$103,430"
+          description="<span
+                class='text-sm font-weight-bolder text-success'
+                >+5%</span> than last month"
+          :icon="{
+            component: 'ni ni-cart',
+            background: 'bg-gradient-warning',
+            shape: 'rounded-circle',
+          }"
+        />
+      </div>
+    </div> -->
 
-      <!-- Card header -->
-      <div @click="deleteEditView" class="my-1 card-header col-8">
-        <div class="text-center">
-          <h2 class="mb-0">무지출 챌린지</h2>
-        </div>
+    <!-- card -->
+    <div class="card col-lg-11 container justify-content-center">
+      <div class="row d-flex">
+        <!-- 카드  -->
 
-        <div class="card-body">
-          <!-- 28개 컴포넌트 -->
-          <div>
-            <MonthChallenge> </MonthChallenge>
-            <br />
-            <!-- 입력창 -->
-            <div
-              class="card p-4"
-              :editChallengeDay="editChallengeDay"
-              style="margin-left: 80px; width: 80%"
-            >
-              <EditChallenge></EditChallenge>
+        <!-- Card header -->
+        <div class="my-1 card-header col-8">
+          <div class="text-center">
+            <h2 class="mb-0">무지출 챌린지</h2>
+          </div>
+
+          <div class="card-body">
+            <!-- 28개 컴포넌트 -->
+            <div>
+              <MonthChallenge :weeks="weeks"> </MonthChallenge>
+              <br />
+              <!-- 입력창 -->
+              <div
+                class="card p-4"
+                :editChallengeDay="editChallengeDay"
+                style="margin-left: 80px; width: 80%"
+              >
+                <EditChallenge></EditChallenge>
+              </div>
             </div>
           </div>
         </div>
+
+        <!-- 사이드바 -->
+        <div class="col-4">
+          <PastChallengeSideBar
+            :pastChallengeList="pastChallengeList"
+          ></PastChallengeSideBar>
+        </div>
       </div>
 
-      <!-- 사이드바 -->
-      <div class="col-4">
-        <PastChallengeSideBar
-          :pastChallengeList="pastChallengeList"
-        ></PastChallengeSideBar>
+      <!-- 아래 -->
+      <div>
+        <AuthorsTable
+          style="width: 80%; margin-left: 50px"
+          :challengeList="challengeList"
+        >
+        </AuthorsTable>
+        <br />
       </div>
-    </div>
-
-    <!-- 아래 -->
-    <div>
-      <AuthorsTable
-        style="width: 80%; margin-left: 50px"
-        :challengeList="challengeList"
-      >
-      </AuthorsTable>
-      <br />
     </div>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+
 import AuthorsTable from "./components/MonthChallengeItem.vue";
 import PastChallengeSideBar from "./components/PastChallengeSideBar.vue";
 import EditChallenge from "./components/EditChallenge.vue";
 import MonthChallenge from "./components/MonthChallenge.vue";
 
-// todo - axios, pinina
+import axios from "axios";
 
+const challengeList = ref([]);
+const pastChallengeList = ref([]);
+const weeks = ref({});
+
+const getPastChallengeList = async () => {
+  try {
+    const response = await axios.get("http://localhost:3001/challenge");
+    const jsonData = response.data;
+    pastChallengeList.value = jsonData;
+  } catch (error) {
+    console.log("Error fetch data ", error);
+  }
+};
+
+const getChallengeDays = async () => {
+  try {
+    const response = await axios.get("http://localhost:3001/challengeDays");
+    const jsonData = response.data;
+    const allChallenges = Object.values(jsonData).flat();
+    console.log(allChallenges);
+    // weeks todo
+    weeks.value = weeks;
+    challengeList.value = allChallenges;
+  } catch (error) {
+    console.log("Error fetch data", error);
+  }
+};
+
+onMounted(() => {
+  getPastChallengeList();
+  getChallengeDays();
+});
+
+// 잘 되는 코드
 // 하단
-const challengeList = ref([
-  {
-    id: 1,
-    name: "챌린지 1",
-    memo: "개휘1",
-    status: true,
-    date: "2024-06-12",
-  },
-  {
-    id: 2,
-    name: "챌린지 2",
-    memo: "개휘",
-    status: false,
-    date: "2024-06-11",
-  },
-]);
+// const challengeList = ref([
+//   {
+//     id: 1,
+//     name: "챌린지 1",
+//     memo: "개휘1",
+//     status: true,
+//     date: "2024-06-12",
+//   },
+//   {
+//     id: 2,
+//     name: "챌린지 2",
+//     memo: "개휘",
+//     status: false,
+//     date: "2024-06-11",
+//   },
+// ]);
 
 // 옆에
-const pastChallengeList = ref([
-  {
-    challengeId: 4,
-    name: "챌린지4 ",
-    date: "2024-06-14",
-  },
-  {
-    challengeId: 3,
-    name: "챌린지 3",
-    date: "2024-06-13",
-  },
-  {
-    challengeId: 2,
-    name: "챌린지 2",
-    date: "2024-06-12",
-  },
-  {
-    challengeId: 1,
-    name: "챌린지 1",
-    date: "2024-06-11",
-  },
-]);
+// const pastChallengeList = ref([
+//   {
+//     challengeId: 4,
+//     name: "챌린지4 ",
+//     date: "2024-06-14",
+//   },
+//   {
+//     challengeId: 3,
+//     name: "챌린지 3",
+//     date: "2024-06-13",
+//   },
+//   {
+//     challengeId: 2,
+//     name: "챌린지 2",
+//     date: "2024-06-12",
+//   },
+//   {
+//     challengeId: 1,
+//     name: "챌린지 1",
+//     date: "2024-06-11",
+//   },
+// ]);
 
 const editChallengeDay = ref({
   day: 1,
