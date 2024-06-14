d9113d9b70a17e9bedbee6c9335502db027c302c
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-06-14 13:12:02 +0900

    곽민지/메인 월별수익 추가

--- tickle-tickle/src/views/main/Main.vue
+++ tickle-tickle/src/views/main/Main.vue
@@ -23,7 +23,7 @@ const loginUser = async () => {
   try {
     const response = await axios.get("http://localhost:3000/users");
     const users = response.data;
-    
+
     // userId와 일치하는 사용자를 찾기
     const user = users.find(user => user.userId === userId);
 
@@ -54,7 +54,7 @@ const fetchChartData = async () => {
 
     // chartData 업데이트 (각 월의 total 값을 사용)
     // chartData.value.datasets[0].data = filteredData.map(item => item.total);
-    
+
     chartData.value = {
       ...chartData.value,
       datasets: [{
@@ -92,18 +92,58 @@ const fetchAnalyticsData = async () => {
   }
 };
 
+// axios를 사용하여 TransactionList.vue에서 계산한 incomeTotal 가져오기
+const fetchTransactionIncomeTotal = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/transactions');
+    if (response.data && response.data.length > 0) {
+      transactions.value = response.data; // 가져온 데이터를 transactions에 할당합니다.
+    }
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+  }
+};
+
+
 // 컴포넌트가 마운트되면 데이터 가져오기
 onMounted(async () => {
   const userName = await loginUser(); // 사용자 정보 가져오기
-  
+
   if (userName) {
     greeting.value = `${userName}님! 안녕하세요`;
   } else {
     greeting.value = '사용자를 찾을 수 없습니다.'; // 사용자를 찾지 못한 경우 처리
   }
-  
+
   fetchChartData();
   fetchAnalyticsData();
+  fetchTransactionIncomeTotal();
+});
+
+const transactions = ref([]);
+
+const incomeTotal = computed(() => {
+  return transactions.value.reduce((total, transaction) => {
+    if (transaction.isIncome) {
+      return total + parseInt(transaction.cost);
+    }
+
+    return total;
+
+  }, 0);
+
+});
+
+const expendTotal = computed(() => {
+  return transactions.value.reduce((total, transaction) => {
+    if (!transaction.isIncome) {
+      return total + parseInt(transaction.cost);
+    }
+
+    return total;
+
+  }, 0);
+
 });
 
 let greeting = ref('로딩 중...');
@@ -118,56 +158,60 @@ const top5AnalyticsData = computed(() => {
 
 <template>
   <div>
-  <br><br>
-  <div class="container">
+    <br><br>
+    <div class="container">
       <h4 style="color: white;">{{ greeting }}</h4>
       <!-- <h5>안녕하세요</h5> -->
     </div>
-  
-  <br>
+
+    <br>
     <div class="container row justify-content-center d-flex">
       <!-- 연간 지출 그래프 쪽 -->
       <div class="container col-8 justify-content-center border border-dark">
         <!-- 연간 지출 그래프  -->
-       
-          
-            
-            <gradient-line-chart
-              id="chart-line"
-              title="연간 지출 그래프"
-              :chart="chartData"
-            />
 
-            <br />
-            
-          
-        
+
+
+        <gradient-line-chart id="chart-line" title="연간 지출 그래프" :chart="chartData" />
+
+        <br />
+
+
+
 
         <br />
         <!-- 지출 카테고리 순위 -->
-        
-            <analytics-card
-              title="월별 지출 카테고리 순위"
-              :headers="['카테고리', '사용 금액']"
-              :pages="top5AnalyticsData"
-            />
-            <br>
-            
-          
+
+        <analytics-card title="월별 지출 카테고리 순위" :headers="['카테고리', '사용 금액']" :pages="top5AnalyticsData" />
+        <br>
+
+
       </div>
 
       <!-- ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ -->
       <!-- 월별 수익 쪽  -->
       <div class="container col-4">
         <div class="card container">
-          <div class="card-body">
+          <div class="card-body text-center mt-3">
             <h4>월별 수익</h4>
+            <p class="card-description mb-4">
+              {{ incomeTotal }} 원
+            </p>
+            
+            <img src="../../assets/img/transIncome.png" class="img-fluid border-radius-lg">
+            
           </div>
         </div>
 
         <div class="card container">
-          <div class="card-body">
+          <div class="card-body text-center mt-3">
             <h4>월별 지출</h4>
+            <p class="card-description mb-4">
+              {{ expendTotal }} 원
+            </p>
+            
+            <img src="../../assets/img/transExpend.png" class="img-fluid border-radius-lg">
+            
           </div>
         </div>
 
